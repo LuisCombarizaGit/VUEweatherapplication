3a738a37ca6aed0e7ee88822544c87599c300906
Luis Combariza <luis_combariza@outlook.com>
2021-02-17 20:24:21 -0700

    Finished css styling , now workig on API communication

--- vue-weather-app/src/App.vue
+++ vue-weather-app/src/App.vue
@@ -4,6 +4,23 @@
       <div class="search-box">
         <input type="text" class="search-bar" placeholder="Search.." />
       </div>
+
+      <div class="weather-wrap">
+        <div class="location-box">
+          <div class="location">
+            Calgary, AB
+            <div class="date">
+              Monday January 20, 2020
+            </div>
+          </div>
+          <div class="weather-box">
+            <div class="temp">
+              9°
+            </div>
+            <div class="weather">Rain</div>
+          </div>
+        </div>
+      </div>
     </main>
   </div>
 </template>
@@ -40,10 +57,9 @@ main {
   padding: 25px;
 }
 .search-box {
-  padding: 80px;
+  padding: 90px;
   display: flex;
   width: 100%;
-  margin-bottom: 30px;
   justify-content: center;
 }
 .search-box .search-bar {
@@ -68,5 +84,36 @@ main {
   box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
   background-color: rgba(255, 255, 255, 0.75);
   border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box {
+  text-align: center;
+}
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+.weather-box .weather {
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
