bce72d2474f9d8ba7e020bf6f377507fe10fcc79
Luis Combariza <luis_combariza@outlook.com>
2021-02-18 12:27:52 -0700

    Added final project code

--- vue-weather-app/src/App.vue
+++ vue-weather-app/src/App.vue
@@ -1,30 +1,36 @@
 <template>
-  <div id="app">
+  <div
+    id="app"
+    :class="
+      typeof weather.main != 'undefined'
+        ? weather.main.temp < 10
+          ? 'cold'
+          : 'warm'
+        : 'background-img.jpg'
+    "
+  >
     <main>
       <div class="search-box">
         <input
           type="text"
           class="search-bar"
-          placeholder="Search.."
+          placeholder="Search..."
           v-model="query"
           @keypress="fetchWeather"
         />
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
           <div class="location">
-            Calgary, AB
-            <div class="date">
-              Monday January 20, 2020
-            </div>
+            {{ weather.name }}, {{ weather.sys.country }}
           </div>
-          <div class="weather-box">
-            <div class="temp">
-              9°
-            </div>
-            <div class="weather">Rain</div>
-          </div>
+          <div class="date">{{ dateBuilder() }}</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
     </main>
@@ -33,7 +39,7 @@
 
 <script>
 export default {
-  name: "App",
+  name: "app",
   data() {
     return {
       api_key: "98b5ba2ee69aa64434fb7d86727ae023",
@@ -42,16 +48,51 @@ export default {
       weather: {},
     };
   },
-
   methods: {
     fetchWeather(e) {
       if (e.key == "Enter") {
         fetch(
-          `${this.api_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
-        ).then((res) => {
-          return;
-        });
+          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+        )
+          .then((res) => {
+            return res.json();
+          })
+          .then(this.setResults);
       }
+    },
+    setResults(results) {
+      this.weather = results;
+    },
+    dateBuilder() {
+      let d = new Date();
+      let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day} ${date} ${month} ${year}`;
     },
   },
 };
@@ -71,6 +112,14 @@ body {
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
+}
+
+#app.warm {
+  background-image: url("./Mild.jpg");
+}
+
+#app.cold {
+  background-image: url("./Cold.jpg");
 }
 
 main {
